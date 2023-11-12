2a15873b3c92d67d7b35efd97fb7eaf094f4fa26
milugarco <nosapien2@gmail.com>
2023-11-12 20:17:15 -0300

    criado componente para input customizado

---
+++ src/components/Input/index.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { TextInput } from "react-native";
+
+const InputCustom = ({ placeholder, backgroundColor, width, marginBottom, marginTop, onChange }) => {
+    const [newValue, setNewValue] = useState();
+
+    const handleChange = (value) => {
+        setNewValue(value);
+        // Chama a função fornecida pelo componente pai para notificar sobre a mudança
+        if (onChange) {
+            onChange(value);
+        }
+    };
+
+    return (
+        <TextInput
+            value={newValue}
+            placeholder={placeholder}
+            style={{
+                backgroundColor: backgroundColor,
+                width: width,
+                fontSize: 15,
+                height: 45,
+                padding: 15,
+                textAlign: 'center',
+                borderRadius: 50,
+                marginBottom: marginBottom ? marginBottom : 0,
+                marginTop: marginTop ? marginTop : 0,
+            }}
+            onChangeText={handleChange}
+        />
+    );
+};
+
+export default InputCustom;
